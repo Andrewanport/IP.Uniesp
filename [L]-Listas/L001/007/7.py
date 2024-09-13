86a0193f9a2de217792896a0c7a736bfec310d23
André Wanderley <154604167+Andrewanport@users.noreply.github.com>
2024-09-13 12:13:02 -0300

    Delete [L]-Listas/L001 directory

--- [L]-Listas/L001/007/7.py
+++
@@ -1,8 +0,0 @@
-"""""
-Crie um programa em Python que pergunte o nome do usuário e imprima um bom dia com o nome do usuário. 
-Dica: você pode utilizar o método .format() ou uma concatenação de string, por exemplo.
-"""""
-
-X = input('Digite o seu nome: ')
-
-print(f'Seja bem vindo, {X}!')
