5198ae89de300230438aae6a5c0ae00f1a6970e8
André Wanderley <154604167+Andrewanport@users.noreply.github.com>
2024-09-13 12:12:33 -0300

    Add files via upload

---
+++ [L]-Listas/L001/003/3.py
@@ -0,0 +1,10 @@
+"""""
+Escreva um programa que solicite ao usuário dois números e imprima a soma deles.
+"""""
+
+N1 = float(input('Digite um número (1): '))
+N2 = float(input('Digite um número (2): '))
+
+S = N1 + N2
+
+print(f'A soma de {N1:.2f} + {N2:.2f} = {S:.2f}')
