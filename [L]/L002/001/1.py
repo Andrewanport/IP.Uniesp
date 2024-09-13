024fea437dbf362be4ba7a421ce320857d94d5a6
André Wanderley <154604167+Andrewanport@users.noreply.github.com>
2024-09-13 12:15:06 -0300

    Delete [L] directory

--- [L]/L002/001/1.py
+++
@@ -1,5 +0,0 @@
-"""""
-Ler as notas da 1a. e 2a. avaliações de um aluno. Calcular a média aritmética simples e escrever uma mensagem que diga 
-se o aluno foi ou não aprovado (considerar que nota igual ou maior a 6 o aluno é aprovado). 
-Escrever também o resultado da média calculada.
-"""""
